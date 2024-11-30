8c853cc9d033eb18cd074a0541981ab23d7ad3cb
Nayeli <nayeli.guzman@utec.edu.pe>
2024-11-30 12:34:57 -0500

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/AuthComponent/Login";
+import Register from "./components/AuthComponent/Register";
 
 
 function App() {
 
   return (
     <>
-      <button className="btn">test</button>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
     </>
   )
 }
